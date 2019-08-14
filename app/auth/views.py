e55fe2d73a492bee6cc122ca9610c3305dfe5fb3
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-14 18:31:03 +0300

    changed line1

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,4 +1,4 @@
-rom flask import render_template,redirect,url_for,flash,request
+from flask import render_template,redirect,url_for,flash,request
 from . import auth
 from flask_login import login_user,logout_user,login_required
 from ..models import User
