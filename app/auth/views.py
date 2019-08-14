a53f7b0940a5a4ced738ed3292207d6d52bba385
emmanuelmuchiri <emmanuel.muchiri@outlook.com>
2019-08-15 01:02:54 +0300

    confirmed that the welcome email message is working perfectly

--- app/auth/views.py
+++ app/auth/views.py
@@ -18,7 +18,7 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "Blog Log in"
+    title = "Stadious Laundry"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
 @auth.route('/register',methods = ["GET","POST"])
@@ -29,7 +29,7 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        mail_message("Welcome to My Blog","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to Stadious Laundry","email/welcome_user",user.email,user=user)
 
         return redirect(url_for('auth.login'))
         title = "New Account"
