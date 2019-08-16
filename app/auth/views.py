9a5aa07478e7540262875e98f22f33f1208f14ab
brayomumo <brayomumo5@gmail.com>
2019-08-16 04:07:48 +0300

    create admin sign in functions

--- app/auth/views.py
+++ app/auth/views.py
@@ -27,6 +27,20 @@ def register():
        return redirect(url_for('auth.login'))
        title = "New Account"
    return render_template('auth/register.html',registration_form = form)
+@auth.route("/admin/login", methods=['GET','POST'])
+def admin_login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.admin'))
+
+        flash('Invalid username or Password')
+    title = "Stadious Laundry Admin login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
+
+
 @auth.route('/logout')
 @login_required
 def logout():
